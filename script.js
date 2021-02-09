f117c35e9b6cd9ec0d91a3dba1ba2830ba0be68f
Nícholas Eras <48137930+Nicholamais@users.noreply.github.com>
2021-02-08 21:19:14 -0300

    v1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.MenuHambur');
 const col = document.querySelector(".coluna");
 let menuOpen = false;
+let menu2Open = false
 
 
 menuBtn.addEventListener('click', ()=> {
@@ -16,4 +17,23 @@ menuBtn.addEventListener('click', ()=> {
     }
 });
 
+
+
+function cor(color){
+    document.body.style.backgroundColor = color;
+}
+
+function cort(cor){
+    document.querySelector('.texto').style.color = cor;
+    document.querySelector('.h1').style.color = cor;
+}
+
+var slider = document.querySelector('.slider');
+var numero = document.querySelector('.numero');
+numero.innerHTML = slider.value;
+
+slider.oninput = function(){
+    numero.innerHTML = this.value + '%';
+    document.body.style.opacity = (this.value)/100;
+}
 
